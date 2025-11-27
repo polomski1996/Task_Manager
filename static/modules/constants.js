1ec45e18666c22150feaa340467eb020b077209f
polomski1996 <polomskimaciej5@gmail.com>
2025-11-27 12:25:59 +0100

    change base url- it is beeing add automatically

--- static/modules/constants.js
+++ static/modules/constants.js
@@ -1,3 +1,3 @@
 // Config and constants.
-export const API_BASE_URL = 'taskmanager-production-4c44.up.railway.app/api';
+export const API_BASE_URL = '/api';
 export const DATE_FORMAT = 'YYYY-MM-DD';
