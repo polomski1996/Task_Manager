73eafea6ccb3aa6d5606059aa706a79237fa2b7a
polomski1996 <polomskimaciej5@gmail.com>
2025-11-29 14:21:30 +0100

    styles for UI

--- Task_Manager/settings/dev.py
+++ Task_Manager/settings/dev.py
@@ -25,32 +25,6 @@ ALLOWED_HOSTS = [
     "*"
 ]
 
-
-# Application definition
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'task_manager',
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-]
-
 # CORS settings for frontend communication
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",  # Your frontend URL
